4940039c0411d732e1c930d43974a55142e91519
Eduardo Silva <eduardo.ejs@hotmail.com>
2018-10-26 11:08:28 -0300

    Bugfix UserRepository - Metodos especificos de create e update - Password Hash

--- bolao/app/Repositories/Eloquent/UserRepository.php
+++ bolao/app/Repositories/Eloquent/UserRepository.php
@@ -3,9 +3,34 @@
 namespace App\Repositories\Eloquent;
 
 use App\User;
+use Illuminate\Support\Facades\Hash;
 use App\Repositories\Contracts\UserRepositoryInterface;
 
 class UserRepository extends AbstractRepository implements UserRepositoryInterface
 {
     protected $model = User::class;
+
+    // Método específico para criar usuario. Definir criptografia no campo senha.
+    // "Sobrescreve o método global em AbstractRepository
+    public function create(array $data):Bool
+    {
+        $data['password'] = Hash::make($data['password']); //criptografando senha antes de criar registro
+        return (bool) $this->model->create($data);
+    }
+
+    public function update(array $data, int $id):Bool
+    {
+        $register = $this->find($id);
+        if($register){
+
+            // Verifica se existe a variavel password no array
+            if($data['password'] ?? false){
+                $data['password'] = Hash::make($data['password']); //criptografando senha antes de editar registro
+            }
+
+            return (bool) $register->update($data);
+        }
+
+        return false;
+    }
 }
