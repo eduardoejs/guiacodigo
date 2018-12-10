469037e7eff8f10d6d44586890d1a17b872fe460
Eduardo Silva <eduardo.ejs@hotmail.com>
2018-12-10 13:51:36 -0200

    Ajustando pagina principal

--- bolao/routes/web.php
+++ bolao/routes/web.php
@@ -24,14 +24,15 @@ Route::get('lang', function () {
     return redirect()->back();
 })->name('set-language');
 
-Route::get('/', function () {
+Route::get('/modelo', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
+Route::namespace('Site')->group(function(){
+    Route::get('/', 'PrincipalController@index')->name('principal');
+});
 
 Route::prefix('admin')->middleware('auth')->namespace('Admin')->group(function(){
     Route::resource('/users', 'UserController');
